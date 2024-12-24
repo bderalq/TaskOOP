7ca708a91186e5fc31bf89ba099bfd466e132aff
baderalqallaf <baderalqallaf@BADERALQALLE446>
2024-12-24 11:23:28 +0000

    Add project files.

---
+++ TaskOOP/Models/Students.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskOOP.Models
+{
+    internal class Students
+    {
+        // student class 
+        public string Student(string name, string major, double gpa)
+        {
+            return "Name is : " + name + 
+                ". Major is : " + major + 
+                ". GPA is : " + gpa;
+        } 
+
+        // inheritance 
+        public string GraduateStudent(string name, string major, double gpa, string dissertation, string award)
+        {
+            return "Name is : " + name +
+                 ". Major is : " + major +
+                ". GPA is = " + gpa +
+                ". Dissertation Title : " + dissertation +
+                ". Award Title : ";
+        }
+    }
+}
---
+++ TaskOOP/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using TaskOOP.Models;
+
+Students stud = new Students();
+string name, major, dissertation, award;
+double gpa;
+
+// ask for name of the student
+Console.WriteLine("Enter name : ");
+name = Console.ReadLine();
+
+// ask for the major 
+Console.WriteLine("Enter Major : ");
+major = Console.ReadLine();
+
+// ask for gpa and convert input from string to double 
+Console.WriteLine("Enter GPA : ");
+gpa = Convert.ToDouble((Console.ReadLine()));
+
+// print student details inclding name, major and gpa.
+Console.WriteLine(stud.Student(name, major, gpa));
+
+// ask for title of dissertation 
+Console.WriteLine("Enter dissertation title : ");
+dissertation = Console.ReadLine();
+
+// ask for award tite 
+Console.WriteLine("Enter Award title : ");
+award = Console.ReadLine();
+
+// print graduate student function using info from student function (inheritance)
+Console.WriteLine(stud.GraduateStudent(name, major, gpa, dissertation, award));
